55b1b7c132f5c60ce87072aa94a637f945bc67c0
natsumi.fujimaki <3785.summer@gmail.com>
2020-12-10 23:39:15 +0900

    Add ShowServlet

---
+++ src/controllers/ShowServlet.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Note;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class ShowServlet
+ */
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Note n = em.find(Note.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("note", n);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/notes/show.jsp");
+        rd.forward(request, response);
+    }
+
+}
