e5f6986cf65ca9c026a7ec0c6fc196f917961ce2
natsumi.fujimaki <3785.summer@gmail.com>
2020-12-10 10:33:58 +0900

    Add getAllNotes

--- src/models/Note.java
+++ src/models/Note.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllNotes",
+            query = "SELECT m FROM Note AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "notes")
 public class Note {
     @Id
